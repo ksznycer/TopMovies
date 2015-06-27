fb5f15315f2555a043ad0e64224fc76e47d1a2c5
ksznycer <sznycerk@gmail.com>
2015-06-27 10:02:17 +0200

    up dll

--- Top20.aspx.cs
+++ Top20.aspx.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Configuration;
+using Npgsql;
 
 public partial class Top20 : System.Web.UI.Page
 {
@@ -12,8 +13,9 @@ public partial class Top20 : System.Web.UI.Page
     {
         using (Npgsql.NpgsqlConnection connection = new Npgsql.NpgsqlConnection(ConfigurationSettings.AppSettings["ConnectionString"]))
         {
-            //connection.ConnectionString = "Server=ec2-54-217-202-110.eu-west-1.compute.amazonaws.com;Port=5432;User Id=iwzexazhfjxbbt;Password=4JVMJFooosyfdM5Y79Si-c691D;Database=d8u6uelvine6d6;Ssl=true";
             connection.Open();
+
+            connection.Close();
         }
     }
 }
